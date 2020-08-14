6c548881eb718f10fd01244a78769d3b03e1aea7
Alicia Mira Kim <alicia.mira.kim@gmail.com>
2020-08-14 16:05:55 -0400

    Starting columnWinInspect to check for wins.

---
+++ columnWinInspector.js
@@ -0,0 +1,24 @@
+
+export class ColumnWinInspector {
+  constructor(column) {
+    this.column = column;
+  }
+  inspect() { // check for 4-token match same player, column
+    // this.column.columnTokens[i]
+      // some kind of for loop, a counter, through the column squares.
+      // what token, count- track matching tokens, return token value
+
+    let match; // ?? boolean???
+
+    
+
+    // count = 0; maybe use this
+
+    // if match, return player, else 0
+    if (match) {
+      return player;
+    } else {
+      return 0;
+    }
+  }
+}
